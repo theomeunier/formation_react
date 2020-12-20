204b286a49662a669cfe6171c47972afc5cf6904
Theo <theo.meunier41@gmail.com>
2020-12-20 20:40:08 +0100

    la syntaxe JSX

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 let n = 0
 
-function render(){
-    const title = React.createElement('h1', {},
-        'boujours tout le monde ',
-        React.createElement('span',  {}, n)
-    )
+function numberFormat(n) {
+    return n.toString().padStart(2, '0')
+}
 
+function render() {
+
+    const items = [
+        'Tache 1',
+        'Tache 2',
+        'Tache 3'
+    ]
+    const title = <React.Fragment>
+        <h1 className="title" id="title">
+            bonjour les gens <span>{numberFormat(n)}</span>
+        </h1>
+        <ul>{items.map((items,k) => <li key={k}>{items}</li>)}</ul>
+    </React.Fragment>
     ReactDOM.render(title, document.querySelector('#app'))
 }
 
 //on appel la function
 render()
 
-window.setInterval(()=>{
+window.setInterval(() => {
     n++
     render()
-},1000)
+}, 1000)
