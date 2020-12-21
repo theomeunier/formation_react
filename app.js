fb81fa598ab38de054923c410a2720fcd4a2467a
Theo <theo.meunier41@gmail.com>
2020-12-21 14:30:29 +0100

    notre premier composant

--- app.js
+++ app.js
@@ -1,29 +1,94 @@
-let n = 0
-
-function numberFormat(n) {
-    return n.toString().padStart(2, '0')
+function WelcomeFunc({name, children}) {
+    return <div>
+        <h1>bonjour {name}</h1>
+        <p>
+            {children}
+        </p>
+    </div>
 }
 
-function render() {
+class Welcome extends React.Component {
 
-    const items = [
-        'Tache 1',
-        'Tache 2',
-        'Tache 3'
-    ]
-    const title = <React.Fragment>
-        <h1 className="title" id="title">
-            bonjour les gens <span>{numberFormat(n)}</span>
-        </h1>
-        <ul>{items.map((items,k) => <li key={k}>{items}</li>)}</ul>
-    </React.Fragment>
-    ReactDOM.render(title, document.querySelector('#app'))
+    render() {
+        return <div>
+            <h1>Bonjours {this.props.name}</h1>
+            <p>
+                {this.props.children}
+            </p>
+        </div>
+    }
 }
 
-//on appel la function
-render()
+class Clok extends React.Component {
 
-window.setInterval(() => {
-    n++
-    render()
-}, 1000)
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()}
+        this.timer = null
+    }
+
+    componentDidMount() {
+        this.timer = window.setInterval(() => {
+            this.tick.bind(this)
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.timer)
+    }
+
+    tick() {
+        this.setState({date: new Date})
+    }
+
+    render() {
+        const date = new Date()
+        return <div>
+            Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
+        </div>
+    }
+}
+
+class Incrementer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {n: props.start}
+        this.timer = null
+    }
+
+    componentDidMount() {
+        window.setInterval(this.increment.bind(this), 1000)
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.timer)
+    }
+
+    increment() {
+        this.setState(function (state, props) {
+            return {n: state.n + props.step}
+        })
+    }
+
+    render() {
+        return <div>Valeur : {this.state.n}</div>
+    }
+}
+
+Incrementer.defaultProps = {
+    start: 0,
+    step: 1,
+}
+
+function Home() {
+    return <div>
+        <Welcome name="Jean"/>
+        <Welcome name="theo"/>
+        <Clok/>
+        <Incrementer start={10}/>
+        <Incrementer start={100} step={10}/>
+    </div>
+}
+
+ReactDOM.render(<Home/>, document.querySelector('#app'))
