b4549beb936bab9a5346e18b056d37babd617551
isox <theo.meunier41@gmail.com>
2020-12-23 01:16:26 +0100

    feat:
    formulaire avec react et mise en place de la soumistion
    ajout de bootstrap

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+class Field extends React.Component {
+    render() {
+        const {name, value, onChange, children} = this.props
+        return <div className="form-group">
+            <label htmlFor={name}>{children}</label>
+            <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
+        </div>
+    }
+}
+
+function Checkbox({name, value, onChange, children}) {
+    return <div className="form-check">
+        <input type="checkbox" checked={value} onChange={onChange} id={name} name={name} className="form-check-input"/>
+        <label htmlFor={name} className="form-check-label">{children}</label>
+    </div>
+}
+
+
 class Home extends React.Component {
 
     constructor(props) {
@@ -8,6 +26,7 @@ class Home extends React.Component {
             newsletter: false
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(e) {
@@ -19,26 +38,30 @@ class Home extends React.Component {
         })
     }
 
+    handleSubmit(e){
+        e.preventDefault()
+        const data = JSON.stringify(this.state)
+        this.setState({
+            nom: '',
+            prenom: '',
+            newsletter: false
+        })
+        console.log(data)
+
+    }
+
     render() {
-        return <div>
-            <div>
-                <label htmlFor="nom">nom</label>
-                <input type="text" id="nom" name="nom" value={this.state.nom} onChange={this.handleChange}/>
-            </div>
-            <div>
-                <label htmlFor="prenom">Prenom</label>
-                <input type="text" id="prenom" name="prenom" value={this.state.prenom} onChange={this.handleChange}/>
-            </div>
-            <div>
-                <label htmlFor="newsletter">S'abonner a la newsletter</label>
-                <input type="checkbox" checked={this.state.newsletter} value={this.state.newsletter}
-                       onChange={this.handleChange} id="newsletter" name="newsletter"/>
-            </div>
-            <div>
-                <input type="text" defaultValue={undefined}/>
+        console.log('render')
+        return <form className="container" onSubmit={this.handleSubmit}>
+            <Field name="nom" value={this.state.nom} onChange={this.handleChange}> Nom </Field>
+            <Field name="prenom" value={this.state.prenom} onChange={this.handleChange}> prénom </Field>
+            <Checkbox name="newsletter" value={this.state.newsletter} onChange={this.handleChange}>S'abonner à la
+                newslatter</Checkbox>
+            <div className="form-group">
+                <button className="btn btn-primary">Envoyer</button>
             </div>
             {JSON.stringify(this.state)}
-        </div>
+        </form>
     }
 }
 
